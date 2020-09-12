2afdc26066333f7b73ee2deea72e761ca30f0bdc
kabu1 <kkabubenson@gmail.com>
2020-09-12 10:21:43 -0400

    add base url

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import article
+Article = article.Article
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
